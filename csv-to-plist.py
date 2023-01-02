27a03ada9b119b7f438aecf6fbdf4d080272715a
chc <106435999+hsiu-chan@users.noreply.github.com>
2023-01-02 17:02:01 +0800

    added 4 files to .

---
+++ csv-to-plist.py
@@ -0,0 +1,23 @@
+from plistlib import dump
+import csv
+
+csv_path='example.csv'
+plist_path=csv_path.split('.')[0]+'.plist'
+
+
+#for line in csv:
+    #pl.append({l[0]:l[1] for l in list(zip(key,#line))})
+
+with open(csv_path, newline='') as csvfile:
+
+    result=[]
+    key=['phrase','shortcut']
+    # 讀取 CSV 檔案內容
+    rows = csv.reader(csvfile)
+
+    # 以迴圈輸出每一列
+    for row in rows:
+        result.append({l[0]:l[1] for l in list(zip(key,row))})
+
+    with open(plist_path, 'wb') as fp:
+        dump(result, fp)
